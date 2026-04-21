59287ef5312f56d22294476a0278ce286188b1cd
abi-cvc <carol.velasquez@epn.edu.ec>
2026-04-20 20:19:23 -0500

    Cambio a JUnit 5, uso de BeforeEach y finalizar pruebas

--- src/test/java/ec/edu/epn/calculator/CalculatorTest.java
+++ src/test/java/ec/edu/epn/calculator/CalculatorTest.java
@@ -1,16 +1,44 @@
 package ec.edu.epn.calculator;
 
-import org.junit.Test;
-
-import junit.framework.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
-    public void testSum(){
-        Calculator calculator = new Calculator();
+    public void testSum() {
         int result = calculator.sum(2, 2);
-        if (result != 4) {
-            Assert.fail();
-        }
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void testMinus() {
+        int result = calculator.minus(2, 2);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testMultiply() {
+        int result = calculator.multiply(3, 5);
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testDivideSuccess() {
+        int result = calculator.divide(10, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testDivideFail() {
+        assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
     }
 }
